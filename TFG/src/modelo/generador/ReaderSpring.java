88f8ae501f076a2389008d71a77561e9c7253c80
TomasGomCas <38647610+TomasGomCas@users.noreply.github.com>
2020-01-06 14:00:21 +0100

    Add Apache Poi Library

--- TFG/src/modelo/generador/ReaderSpring.java
+++ TFG/src/modelo/generador/ReaderSpring.java
@@ -1,12 +1,36 @@
 package modelo.generador;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import interfaces.Reader;
 
 public class ReaderSpring implements Reader{
 
+	private File file = new File("TFG\\target\\excel");
+	
 	@Override
 	public void read() {
-		// TODO Auto-generated method stub
+
+        FileInputStream excelFile;
+		try {
+			excelFile = new FileInputStream(file);
+	        Workbook workbook = new XSSFWorkbook(excelFile);
+	        Sheet datatypeSheet = (Sheet) workbook.getSheetAt(0);
+	        
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
